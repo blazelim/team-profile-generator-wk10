85eb146ec6ef808fba9a148867348a3163548fb7
Blaze Lim <blazebentleycolim@gmail.com>
2021-04-19 21:11:43 -0700

    added employee parent object creator with appropriate tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require('../lib/Employee')
+
+
+test('creates an employee object', () => {
+    const employee = new Employee('Dave', 12345, 'sample@email.com');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test("getName() returns 'this persons name is <name>'", () => {
+    const employee = new Employee('Dave', 12345, 'sample@email.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
+});
+
+test("getId() returns string containing id", () => {
+    const employee = new Employee('Dave', 12345, 'sample@email.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+
+test("getEmail() returns string containing email", () => {
+    const employee = new Employee('Dave', 12345, 'sample@email.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test("getRole() returns Employee", () => {
+    const employee = new Employee('Dave', 12345, 'sample@email.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
